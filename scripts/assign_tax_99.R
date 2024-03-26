ca6ec4991e48288459321c45b7e02b2ee9fc105d
JCSzamosi <szamosjc@mcmaster.ca>
2024-03-26 15:41:36 -0400

    run assign_tax_99 on alpsr4

--- scripts/assign_tax_99.R
+++ scripts/assign_tax_99.R
@@ -1,30 +1,41 @@
-library(tidyverse)
-library(DECIPHER)
+library(multidplyr)
+library(dplyr, warn.conflicts = FALSE)
 library(dada2)
-library(phyloseq)
 
 load('intermed/clst.Rdata')
 
+# Create computer core clusters for parallel processing, and sub-divide data
+cores = new_cluster(20)
+
 # A function to convert a set of sequences into a consensus sequence
-get_conseq = function(x){
-    if (length(x) > 1){
-        aln = AlignSeqs(DNAStringSet(x))
-    } else {
-        aln = DNAStringSet(x)
-    }
-    con = ConsensusSequence(aln)
-    return(as.character(con))
-}
+## create it in each cluster
+cluster_assign(cores, 
+	get_conseq = function(x){
+    	if (length(x) > 1){
+        	aln = AlignSeqs(DNAStringSet(x))
+	    } else {
+    	    aln = DNAStringSet(x)
+	    }
+    	con = ConsensusSequence(aln)
+	    return(as.character(con))
+	}	
+)
+
+cluster_library(cores, 'DECIPHER')
+
+clsts1 = (clsts
+			%>% group_by(cluster)
+			%>% partition(cores))
 
 # Create the consensus sequences for each cluster
-conseq = (clsts
-          %>% group_by(cluster)
-          %>% summarize(consensus = get_conseq(seqs)))
+conseq = (clsts1
+          %>% summarize(consensus = get_conseq(seqs))
+		  %>% collect())
 
 # Assign taxonomy to the consensus sequences
 clstax = assignTaxonomy(conseq$consensus, 
-                        refFasta = '~/Disk2/CommonData/SILVA/silva_nr99_v138_wSpecies_train_set.fa',
-                        tryRC = TRUE, multithread = 8)
+                        refFasta = 'data/silva_nr99_v138_wSpecies_train_set.fa',
+                        tryRC = TRUE, multithread = 20)
 
 
 save(conseq, clstax, file = 'intermed/tax_99.Rdata')

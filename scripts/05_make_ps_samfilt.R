d9f972ec8ee251f1d0eed8e12db14a9140a71aa2
JCSzamosi <szamosjc@mcmaster.ca>
2025-08-26 14:16:49 -0400

    working on making a sample-filtered dataset

--- scripts/05_make_ps_samfilt.R
+++ scripts/05_make_ps_samfilt.R
@@ -3,16 +3,36 @@ library(phyloseq)
 load('cleaned/ps_full.Rdata')
 
 cat('\nThe distribution of read depths is:\n')
-print(summary(sample_sums(ps))
+print(summary(sample_sums(ps)))
 
 cat('\nThe properties of the phyloseq object are:\n')
 print(ps)
 
 cat('\nRemoving negative controls\n')
-ps_samfilt = subset_samples(
-ps_samfilt = prune_samples(sample_sums(ps) >= 10000, ps)
+ps_samfilt = subset_samples(ps, startsWith(as.character(Study.ID), 'IMG'))
+ns = nsamples(ps_samfilt)
+cat(paste('\nRemoved negative controls. ', as.character(ns),
+			' samples remaining.\n', sep = ''))
+
+cat('\nRemoving samples with < 10,000 reads.\n')
+ps_samfilt = prune_samples(sample_sums(ps_samfilt) >= 10000, ps_samfilt)
+ns1 = nsamples(ps_samfilt)
+cat(paste('\nRemoved low-read samples. ', as.character(ns1),
+			' out of ', as.character(ns), ' samples remaining, or ',
+			as.character(round(ns1/ns, 4) * 100), '%.\n',
+			sep = ''))
+
+cat('\nRemoving taxa with 0 counts.\n')
+nt = ntaxa(ps_samfilt)
 ps_samfilt = prune_taxa(taxa_sums(ps_samfilt) > 0, ps_samfilt)
+nt_1 = ntaxa(ps_samfilt)
+cat(paste('\nRemoved 0-count taxa. ', as.character(nt_1), ' out of ',
+			as.character(nt), ' taxa remaining, or ',
+			as.character(round(nt_1/nt, 4) * 100), '%.\n',
+			sep = ''))
 seqs_samfilt = seqs[taxa_names(ps_samfilt)]
+
+cat('\nWriting files.\n')
 
 save(ps_samfilt, file = 'cleaned/ps_samfilt.Rdata')
 save(seqs_samfilt, file = 'cleaned/seqs_samfilt.Rdata')

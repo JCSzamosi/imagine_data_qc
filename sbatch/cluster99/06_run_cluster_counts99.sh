98199d1dc56fd6e0f9b7bb7409b153e3a9714c7f
JCSzamosi <szamosjc@mcmaster.ca>
2025-08-29 14:21:35 -0400

    fix cluster count sbatch

--- sbatch/cluster99/06_run_cluster_counts99.sh
+++ sbatch/cluster99/06_run_cluster_counts99.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=5
-#SBATCH --mem=200G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=150G
 #SBATCH --output=logs/%x_slurm_%j.log
 #SBATCH --error=logs/%x_slurm_%j.log
 #SBATCH --job-name=06_cluster_counts99
@@ -10,15 +10,6 @@
 
 echo module load r/4.5.0
 module load r/4.5.0
-
-echo module load gcc/13.3
-module load gcc/13.3
-
-echo export R_LIBS=~/R/x86_64-pc-linux-gnu-library/4.5/
-export R_LIBS=~/R/x86_64-pc-linux-gnu-library/4.5/
-
-echo export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
 
 echo Rscript ./scripts/cluster99/06_cluster_counts99.R
 Rscript ./scripts/cluster99/06_cluster_counts99.R

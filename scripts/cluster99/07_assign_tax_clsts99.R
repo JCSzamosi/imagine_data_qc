4f9e8d8f61582dca5627945b88e6408f0e9b44a5
JCSzamosi <szamosjc@mcmaster.ca>
2025-09-08 11:29:29 -0400

    working on assigning cluster taxonomy

--- scripts/cluster99/07_assign_tax_clsts99.R
+++ scripts/cluster99/07_assign_tax_clsts99.R
@@ -7,11 +7,27 @@ library(dada2)
 indir = 'intermed'
 conseqf = 'conseqs99.csv'
 outdir = 'cleaned'
-outf = 'clstaxtab99.rds'
+outf = 'clstaxtab99.csv'
 
 # Read in the data ####
 
 cat('\nRead in the consensus sequences\n')
 
 conseq = read.csv(file = file.path(indir, conseqf),
-                  row.names = 1)
+                  row.names = 1, header = TRUE)
+
+seqs = conseq$consensus
+
+cat('\nCheck sequences\n')
+if (length(unique(seqs))/length(seqs) != 1){
+    msg = 'The consensus sequences are not all unique'
+    stop(msg)
+}
+cat('\nStart assigning taxonomy\n')
+taxtab = assignTaxonomy(seqs,
+                        refFasta = 'refs/silva_nr99_v138_train_set.fa',
+                        tryRC = TRUE, multithread = 40, verbose = TRUE)
+
+outp = file.path(outd, outf)
+cat('\nWrite tax table')
+cat('\nDone\n')

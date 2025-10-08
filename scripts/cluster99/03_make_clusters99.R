1ba5407356941ea06cffbe9b7a508ba3dae39246
JCSzamosi <szamosjc@mcmaster.ca>
2025-10-08 17:07:06 -0400

    Testing out two different clustering methods

--- scripts/cluster99/03_make_clusters99.R
+++ scripts/cluster99/03_make_clusters99.R
@@ -6,10 +6,12 @@ library(DECIPHER)
 
 outdir = 'intermed'
 outfnm = 'clsts99.Rdata'
+outfnm2 = 'clsts99_notail.Rdata'
 indir = 'cleaned/asvs/full/'
 inseq = 'full_seqs.Rdata'
 inf = file.path(indir, inseq)
 outf = file.path(outdir, outfnm)
+outf2 = file.path(outdir, outfnm2)
 
 cat('\nLoad the sequences\n')
 
@@ -23,6 +25,10 @@ cat('\nStart clustering sequences\n')
 
 clsts = Clusterize(DNAStringSet(seqs), cutoff = 0.01,
                    includeTerminalGaps = TRUE,
+									 processors = 40)
+clsts2 = Clusterize(DNAStringSet(seqs), cutoff = 0.01,
+                   includeTerminalGaps = TRUE,
+                   penalizeGapLetterMatches = TRUE,
 									 processors = 40)
 	# Each sequence has its own row, in the order they were originally listed in
 	# The cluster number that each sequence belongs to is listed in its row in
@@ -40,16 +46,20 @@ cat('\nWriting the cluster data frame\n')
 if (!dir.exists(outdir)){
 	dir.create(outdir, recursive = TRUE)
 }
-save('clsts', file = outf)
+save(clsts, file = outf)
+save(clsts2, file = outf2)
 
 cat('\nWriting Track Stats\n')
 
-stats_df = data.frame(Step = 'cluster99/03_make_clusters.R',
-						Samples = c(NA,NA),
+stats_df = data.frame(Step = c(rep('cluster99/03_make_clusters.R',2),
+                               rep('cluster99/03_make_clusters2.R',1)),
+						Samples = rep(NA,3),
 						Taxa = c(nrow(clsts),
-								length(unique(clsts$cluster))),
+								length(unique(clsts$cluster)),
+								length(unique(clsts2$cluster))),
 						File = c(paste(outf,'- ASVs'),
-								paste(outf, '- clusters')))
+								paste(outf, '- clusters'),
+								paste(outf2, '- clusters')))
 
 write.table(stats_df, file = 'stats/track_counts.csv',
 			append = TRUE, quote = TRUE, sep = ',',

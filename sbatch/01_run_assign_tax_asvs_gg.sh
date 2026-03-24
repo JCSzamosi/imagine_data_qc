faa8ae6d7bcb8d95b9f7f0c05db50562533d98c3
JCSzamosi <szamosjc@mcmaster.ca>
2026-03-24 14:13:45 -0400

    run taxonomy

--- sbatch/01_run_assign_tax_asvs_gg.sh
+++ sbatch/01_run_assign_tax_asvs_gg.sh
@@ -4,12 +4,12 @@
 #SBATCH --mem=35G
 #SBATCH --output=logs/%x_slurm_%j.log
 #SBATCH --error=logs/%x_slurm_%j.log
-#SBATCH --job-name=01_assign_tax_asvs
+#SBATCH --job-name=01_assign_tax_asvs_gg
 #SBATCH --time=00:45:00
 #SBATCH --account=rrg-surette
 
 echo module load r/4.5.0
 module load r/4.5.0
 
-echo Rscript ./scripts/01_assign_tax_asvs.R
-Rscript ./scripts/01_assign_tax_asvs.R
+echo Rscript ./scripts/01_assign_tax_asvs.R -d gg
+Rscript ./scripts/01_assign_tax_asvs.R -d gg
---
+++ sbatch/01_run_assign_tax_asvs_silva.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=30
+#SBATCH --mem=35G
+#SBATCH --output=logs/%x_slurm_%j.log
+#SBATCH --error=logs/%x_slurm_%j.log
+#SBATCH --job-name=01_assign_tax_asvs_silva
+#SBATCH --time=00:45:00
+#SBATCH --account=rrg-surette
+
+echo module load r/4.5.0
+module load r/4.5.0
+
+echo Rscript ./scripts/01_assign_tax_asvs.R -d silva
+Rscript ./scripts/01_assign_tax_asvs.R -d silva

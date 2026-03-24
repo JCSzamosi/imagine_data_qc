03a60d963e7b15f4fec1b0b1249db2218fc5ca7a
Jake Szamosi <szamosjc@mcmaster.ca>
2026-03-24 11:28:39 -0400

    Make taxon assignment take a db arg

--- scripts/01_assign_tax_asvs.R
+++ scripts/01_assign_tax_asvs.R
@@ -2,16 +2,34 @@
 
 library(dada2)
 library(readr)
+library(optparse)
 
 # Define I/O Variables
+
+opts = list(
+    make_option(c('-d', '--db'), type = 'character', default = 'silva',
+                help = 'Either \'silva\' or \'gg\'. Defines the database.')
+)
+
+opt_parser <- OptionParser(option_list = opts)
+# Parse the arguments
+opt <- parse_args(opt_parser)
+db = opt$db
 
 datdir = 'data'
 seqfile = 'merged_seqtab.rds'
 inf = file.path(datdir, seqfile)
 refd = 'refs'
-refdb = 'silva_nr99_v138_train_set.fa'
+if (db == 'silva'){
+    refdb = 'silva_nr99_v138_train_set.fa'
+} else if (db == 'gg'){
+    refdb = 'gg2_2024_09_toGenus_trainset.fa'
+} else {
+    stop('Database must be one of "silva" or "gg".')
+}
+
 ref = file.path(refd, refdb)
-outf = 'merged_taxtab.rds'
+outf = sprintf('merged_taxtab_%s.rds', db)
 
 # Load data
 
@@ -42,7 +60,7 @@ write_rds(taxtab, file = outp)
 
 cat('\nWrite tracking stats\n')
 
-stats_df = data.frame(Step = '01_assign_tax_asvs.R',
+stats_df = data.frame(Step = sprintf('01_assign_tax_asvs.R: %s', db),
 					Samples = NA,
 					Taxa = nrow(taxtab),
 					File = outp)
